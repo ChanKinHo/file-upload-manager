c6aed8871b1405f0fdc971e2083a294f95970d07
ChanKinHo <876204671@qq.com>
2022-05-09 07:08:42 +0800

    dubbo提供者启动成功

---
+++ common-interface/src/main/java/com/house/demo/service/HelloService.java
@@ -0,0 +1,6 @@
+package com.house.demo.service;
+
+public interface HelloService {
+
+    public String hello(String msg);
+}
---
+++ file-upload-worker/src/main/java/com/house/demo/service/impl/HelloServiceImpl.java
@@ -0,0 +1,10 @@
+package com.house.demo.service.impl;
+
+import com.house.demo.service.HelloService;
+
+public class HelloServiceImpl implements HelloService {
+    @Override
+    public String hello(String msg) {
+        return null;
+    }
+}
